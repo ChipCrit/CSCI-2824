6d8ec45b637db1cac098475a93596185c68a3815
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 14:47:09 -0600

    sol

--- bitoperator.py
+++ bitoperator.py
@@ -3,8 +3,7 @@ class BitOperator(object):
     def bitOperator(self,bit1,bit2):
         x = int(bit1)
         y = int(bit2)
-        for i in range(0,5):
-            x[i]=x or y
-            y[i]=x and y
-        return x,y
+        sol1 = x|y
+        sol2= x&y
+        return sol1,sol2
 #Start writing your code from here
