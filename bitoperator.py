7da20af9bfb7a48bb8d6ba3ab993ee0c4538c493
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 14:16:39 -0600

    change of tactic

--- bitoperator.py
+++ bitoperator.py
@@ -1,11 +1,10 @@
 #DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
 class BitOperator(object):
     def bitOperator(self,bit1,bit2):
-        x = int(bit1)
-        printf(bit1)
+        x = int(bit1)   
         y = int(bit2)
-        printf(bit2)
         return x|y, x&y
 
 #Start writing your code from here
     
+BitOperator(10001,11011)
