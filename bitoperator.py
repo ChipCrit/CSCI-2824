de020f8aec9abb6ebe87790ed14bfeb0759bcd65
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 14:29:35 -0600

    reorder

--- bitoperator.py
+++ bitoperator.py
@@ -1,8 +1,10 @@
 #DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
 class BitOperator(object):
     def bitOperator(self,bit1,bit2):
-        x = int(bit1)   
-        y = int(bit2)
-        return x|y, x&y
+        x = int(bit1) | int(bit2) 
+        y = int(bit1) & int (bit2)
+        return x,y
+
+        
 
 #Start writing your code from here
