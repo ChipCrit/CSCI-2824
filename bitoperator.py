34637597ca250749f179c87256442468d256280d
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 14:40:19 -0600

    loops

--- bitoperator.py
+++ bitoperator.py
@@ -1,10 +1,11 @@
 #DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
 class BitOperator(object):
     def bitOperator(self,bit1,bit2):
-        x = int(bit1,2) | int(bit2,2) 
-        y = int(bit1,2) & int (bit2,2)
-        return bin(x),bin(y)
-
-        
-
+        x = int(bit1)
+        y = int(bit2)
+        i=int(0)
+        for i in range(0,5):
+            x[i]=x or y
+            y[i]=x and y
+        return x,y
 #Start writing your code from here
