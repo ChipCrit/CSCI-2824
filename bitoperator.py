d0d8c4f13096c1339b19958a9eb92e4b54a2ea4c
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 13:54:04 -0600

    setup

---
+++ bitoperator.py
@@ -0,0 +1,7 @@
+#DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
+class BitOperator(object):
+    def bitOperator(self,bit1,bit2):
+        return bit1|bit2, bit1&bit2
+
+#Start writing your code from here
+    
