a4df376891b7333babb68ac2ad30a56c387b9fa4
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 14:42:34 -0600

    delete int i line

--- bitoperator.py
+++ bitoperator.py
@@ -3,7 +3,6 @@ class BitOperator(object):
     def bitOperator(self,bit1,bit2):
         x = int(bit1)
         y = int(bit2)
-        i=int(0)
         for i in range(0,5):
             x[i]=x or y
             y[i]=x and y
