93e7931e6f9e2071c1550fbc2d1908000e3e1056
Matthew Mayer <matthewmayer@comcast.net>
2021-08-28 14:48:35 -0600

    x and y as binarys

--- bitoperator.py
+++ bitoperator.py
@@ -1,8 +1,8 @@
 #DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
 class BitOperator(object):
     def bitOperator(self,bit1,bit2):
-        x = int(bit1)
-        y = int(bit2)
+        x = bin(bit1)
+        y = bin(bit2)
         sol1 = x|y
         sol2= x&y
         return sol1,sol2
