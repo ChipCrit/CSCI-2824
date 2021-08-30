ca2a2aa897d18e144dd2a9d4ff722b61f995ffba
Matthew Mayer <matthewmayer@comcast.net>
2021-08-29 23:04:30 -0600

    +=

--- bitoperator.py
+++ bitoperator.py
@@ -1,19 +1,19 @@
 #DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
 class BitOperator(object):
     def bitOperator(self,bit1,bit2):
-        x= bit1
-        y= bit2
+        x=""
+        y=""
         for i in range (5):
             if bit1[i]==bit2[i]and bit2[i]=='1':
-                y[i]=1
+                y+="1"
             else:
-                y[i]=0
+                y+="0"
             if bit1[i] == '1':
-                x[i]=1
+                x+="1"
             elif bit2[i] =='1':
-                x[i]=1
+                x+="1"
             else:
-                x[i]=0
+                x+="0"
         xf=int(x)
         yf=int(y)
 
