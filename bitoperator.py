61523b649b4a671e9b52927229681d4431461258
Matthew Mayer <72530028+ChipCrit@users.noreply.github.com>
2021-08-29 23:20:49 -0600

    Update bitoperator.py
    set up correctly

--- bitoperator.py
+++ bitoperator.py
@@ -1,1 +1,17 @@
-
+#DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
+class BitOperator(object):
+    def bitOperator(self,bit1,bit2):
+        x=""
+        y=""
+        for i in range (5):
+            if bit1[i]==bit2[i]and bit2[i]=="1":
+                y+="1"
+            else:
+                y+="0"
+            if bit1[i] == "1":
+                x+="1"
+            elif bit2[i] =="1":
+                x+="1"
+            else:
+                x+="0"
+        return x,y
