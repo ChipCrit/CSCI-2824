e4abe9ecd4b68c953c0ede9055fdab96e2ae2b95
Matthew Mayer <matthewmayer@comcast.net>
2021-09-16 20:03:22 -0600

    2

--- cartesian.py
+++ cartesian.py
@@ -1,21 +1,10 @@
+import itertools
 class CartesianProduct:
     def get_cartesian_product(self,A,B,C):
-        '''
-        input: A,B,C are three input sets
-        output: A * B * C
-        '''
-        lists = [[A],
-                 [B],
-                 [C]]
-        final = []
-        for i in range (0,len(A)):
-            for j in range (0,len(B)):
-                for k in range (0,len(C)):
-                    if type(A[i]) != list:
-                        A[i] = lists[i]
-
-
-                    temp = [num for num in A[i]]
-                    temp.append(B[j])
-                    final.append(temp)
-        return final
+        lists = [
+            [A],
+            [B],
+            [C]
+        ]
+        for element in itertools.product([A],[B],[C]):
+            print(element)
