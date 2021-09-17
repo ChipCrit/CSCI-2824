bfe24fd057775cea3bd34bf09ebe7c47b93cc25f
Matthew Mayer <matthewmayer@comcast.net>
2021-09-16 19:42:15 -0600

    attempt 1

--- cartesian.py
+++ cartesian.py
@@ -4,4 +4,18 @@ class CartesianProduct:
         input: A,B,C are three input sets
         output: A * B * C
         '''
-        
+        lists = [[A],
+                 [B],
+                 [C]]
+        final = []
+        for i in range (0,len(A)):
+            for j in range (0,len(B)):
+                for k in range (0,len(C)):
+                    if type(A[i]) != list:
+                        A[i] = lists[i]
+
+
+                    temp = [num for num in A[i]]
+                    temp.append(B[j])
+                    final.append(temp)
+        return final
