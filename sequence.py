67c390120fa225a4b3253649542d69367adc8448
Matthew Mayer <matthewmayer@comcast.net>
2021-09-25 23:04:26 -0600

    More rounding!

--- sequence.py
+++ sequence.py
@@ -5,8 +5,11 @@ class Sequence():
         round(difference,2)
         if((input_sequence[2]-input_sequence[1]==difference)):
             num=input_sequence[-1]+difference
+            round(num,2)
             return num
         else:
             r=input_sequence[1]/input_sequence[0]
+            round(r,2)
             difference=input_sequence[-1]*r
+            round(difference,2)
             return difference
