9fc5465bba23a61a8410e620d4af9af742d0c343
Matthew Mayer <matthewmayer@comcast.net>
2021-09-25 23:02:58 -0600

    rounding earlier

--- sequence.py
+++ sequence.py
@@ -2,10 +2,11 @@ class Sequence():
     def next_in_sequence(self, input_sequence):
         difference = 0
         difference = input_sequence[1]-input_sequence[0]
+        round(difference,2)
         if((input_sequence[2]-input_sequence[1]==difference)):
             num=input_sequence[-1]+difference
-            return float(round(num,2))
+            return num
         else:
             r=input_sequence[1]/input_sequence[0]
             difference=input_sequence[-1]*r
-            return float(round(difference,2))
+            return difference
